26b0b8b05534e343f14c447b08dc39494b721883
Sploder12 <trevorschupbach@gmail.com>
2022-09-07 09:54:19 -0400

    Update parser.js

--- src/parser.js
+++ src/parser.js
@@ -24,7 +24,7 @@ const c_funcs = {
 
 export function parse(content) {
     
-    content = content.replace(/\r|\n\r/g, '\n'); // thanks again JS
+    content = content.replace(/\r|(\r\n)/g, '\n').replace(/\n*/g, '\n'); // thanks again JS
     const sections = content.split("\n---\n");
     if (sections.length != 2) {
         error("Invalid .dl format.");
@@ -37,7 +37,7 @@ export function parse(content) {
     let node_map = {};
 
     const node_data = nodes.split('\n');
-    for (const node of node_data) {
+    for (let node of node_data) {
         const data = node.split(/\s*\|\s*/);
 
         if (data === null || data.length != 4) {
@@ -58,7 +58,7 @@ export function parse(content) {
         node_map[id] = new struct.Node(name, desc, type, 0.0, 0.0);
     }
     
-    for (connection of connections.strip('\n').split('\n')) {
+    for (let connection of connections.strip('\n').split('\n')) {
         var data = [];
 
         let re = /^(\d*)\s*([^\s]*)\s*(\d*)\s*$/;
